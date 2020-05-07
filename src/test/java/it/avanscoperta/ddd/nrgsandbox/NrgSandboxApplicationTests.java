ba21838524da47625ace6380ee7bfe237dda4e71
Alberto Brandolini <alberto.brandolini@gmail.com>
2020-05-07 09:05:26 +0200

    Initial commit

---
+++ src/test/java/it/avanscoperta/ddd/nrgsandbox/NrgSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package it.avanscoperta.ddd.nrgsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NrgSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
